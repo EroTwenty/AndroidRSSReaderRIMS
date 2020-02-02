ff7f6025444407a2ac366d3ae120092817ff280b
Lorenzo Verbrugghe <enzo.59370@hotmail.fr>
2020-02-02 16:58:05 +0100

    Added Rss settings and reload buttons in layout, starting RSS Xml reading

--- src/main/java/com/example/androidrssreader/MainActivity.java
+++ src/main/java/com/example/androidrssreader/MainActivity.java
@@ -2,12 +2,49 @@ package com.example.androidrssreader;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
+    protected String RSSURL = new String();
+    protected ImageButton settingsButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        RSSURL="https://www.jeuxvideo.com/rss/rss-ps4.xml";
+        settingsButton = (ImageButton) findViewById(R.id.rssSettingsButton);
+        settingsButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                readFromUrl(RSSURL);
+            }
+        });
+
+
     }
+    public void readFromUrl(String inurl){
+        try {
+            URL url = new URL(inurl);
+
+            BufferedReader in = new BufferedReader(
+                    new InputStreamReader(url.openStream()));
+
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) {
+                System.out.println(inputLine);
+            }
+            in.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
 }
